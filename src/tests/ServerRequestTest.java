51c18d35da4044f369863d79c707c491e50940e2
Martin Angelov <martin_angelov@epam.com>
2017-01-10 17:17:58 +0200

    Finish ServerRequestTest

--- src/tests/ServerRequestTest.java
+++ src/tests/ServerRequestTest.java
@@ -2,20 +2,17 @@ package tests;
 
 import static org.powermock.api.mockito.PowerMockito.*;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Formatter;
-import java.util.Iterator;
-import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.api.support.membermodification.MemberModifier;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -61,6 +58,37 @@ public class ServerRequestTest {
 "</body>"+
 "</html>";
 
+	private static final String TEST_RESPONSE_CHUNKED = "HTTP/1.1 200 OK\r\n"+
+"Date: Mon, 22 Mar 2004 11:15:03 GMT\r\n"+
+"Content-Type: text/html\r\n"+
+"Transfer-Encoding: chunked\r\n"+
+"Trailer: Expires\r\n\r\n"+
+
+"4\r\n"+
+"Wiki\r\n"+
+"5\r\n"+
+"pedia\r\n"+
+"E\r\n"+
+" in\r\n"+
+"\r\n"+
+"chunks.\r\n"+
+"0\r\n"+
+"\r\n";
+
+	private static final String TEST_RESPONSE_LENGHTED = "HTTP/1.1 200 OK\r\n"+
+"Date: Mon, 27 Jul 2009 12:28:53 GMT\r\n"+
+"Server: Apache/2.2.14 (Win32)\r\n"+
+"Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\r\n"+
+"Content-Length: 88\r\n"+
+"Content-Type: text/html\r\n\r\n"+
+"<html>\r\n"+
+"   <body>\r\n"+
+"\r\n"+
+"   <h1>Hello, World!</h1>\r\n"+
+"\r\n"+
+"   </body>\r\n"+
+"</html>";
+
 	private static final int HTTP_PORT = 80;
 
 	@Mock
@@ -77,16 +105,19 @@ public class ServerRequestTest {
 
 	private ReadingUtil readingUtil = new ReadingUtil();
 
-	@Test
-	public void testSend() throws Exception {
+	@Before
+	public void prepare() throws Exception {
 		when(userRequestMock.getHost()).thenReturn(TEST_HOST);
 		when(userRequestMock.getRequestRaw()).thenReturn(TEST_REQUEST);
-		when(socketMock.getInputStream()).thenReturn(inputStreamMock);
-		when(inputStreamMock.read()).then(new DataLetterByLetter(TEST_RESPONSE));
-
 		PowerMockito.whenNew(Socket.class).withArguments(TEST_HOST, HTTP_PORT).thenReturn(socketMock);
 		PowerMockito.whenNew(Formatter.class).withParameterTypes(OutputStream.class)
 		.withArguments(null).thenReturn(formatterMock);
+		when(socketMock.getInputStream()).thenReturn(inputStreamMock);
+	}
+
+	@Test
+	public void testSend() throws Exception {
+		when(inputStreamMock.read()).then(new DataLetterByLetter(TEST_RESPONSE));
 
 		ServerRequest toTest = new ServerRequest(userRequestMock);
 
@@ -96,5 +127,33 @@ public class ServerRequestTest {
 		ServerResponse response = toTest.send();
 
 		Assert.assertEquals(response.getResponseRaw(), TEST_RESPONSE_EXPECTED);
+	}
+
+	@Test
+	public void testSendWithChunkedResponse() throws IOException, IllegalArgumentException, IllegalAccessException {
+		when(inputStreamMock.read()).then(new DataLetterByLetter(TEST_RESPONSE_CHUNKED));
+
+		ServerRequest toTest = new ServerRequest(userRequestMock);
+
+		MemberModifier.field(ServerRequest.class, "readingUtil").set(
+				toTest , readingUtil);
+
+		ServerResponse response = toTest.send();
+
+		Assert.assertEquals(response.getResponseRaw(), TEST_RESPONSE_CHUNKED);
+	}
+
+	@Test
+	public void testSendWithLenghtedResponse() throws IOException, IllegalArgumentException, IllegalAccessException {
+		when(inputStreamMock.read()).then(new DataLetterByLetter(TEST_RESPONSE_LENGHTED));
+
+		ServerRequest toTest = new ServerRequest(userRequestMock);
+
+		MemberModifier.field(ServerRequest.class, "readingUtil").set(
+				toTest , readingUtil);
+
+		ServerResponse response = toTest.send();
+
+		Assert.assertEquals(response.getResponseRaw(), TEST_RESPONSE_LENGHTED);
 	}
 }
