97bd7147d3ecbfbd2f3217632338411df2776aac
Martin Angelov <martin_angelov@epam.com>
2017-01-12 18:23:25 +0200

    Caching stubs

--- src/proxyserver5/cache/Evictor.java
+++ src/proxyserver5/cache/Evictor.java
@@ -1,5 +1,20 @@
 package proxyserver5.cache;
 
 public class Evictor {
+	private ProxyCache cache;
 
+	private void evict() {
+		evictETags();
+		evictMaxAge();
+	}
+
+	private void evictETags() {
+	}
+
+	private void evictMaxAge() {
+	}
+
+	public void start() {
+		
+	}
 }
--- src/proxyserver5/cache/ProxyCache.java
+++ src/proxyserver5/cache/ProxyCache.java
@@ -1,5 +1,16 @@
 package proxyserver5.cache;
 
-public class ProxyCache {
+import java.util.HashMap;
+import java.util.Map;
 
+import proxyserver5.ServerResponse;
+
+public class ProxyCache {
+	private Map<String, ServerResponse> eTagMap;
+	private Map<ServerResponse, Long> expirationData;
+
+	public ProxyCache() {
+		eTagMap = new HashMap<>();
+		expirationData = new HashMap<>();
+	}
 }
